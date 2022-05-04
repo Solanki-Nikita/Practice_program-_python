36d9bbfad4a6fe46235f912eb7394ea0ab86ceeb
Solanki-Nikita <90173555+Solanki-Nikita@users.noreply.github.com>
2022-05-04 21:34:33 +0530

    Add files via upload

---
+++ import.pye.py
@@ -0,0 +1,25 @@
+'''import math
+x=int(input("Enter a number"))
+y=math.sqrt(x)
+print (x)
+print (y)
+
+
+import math  as m
+x=int(input("Enter a number"))
+y=m.sqrt(x)
+print(x)
+print(y)
+
+
+
+from math import sqrt
+x=int(input("Enter a number"))
+y=sqrt(x)
+print(x,y)'''
+
+
+from math import *
+x=int(input("Enter a number"))
+y=sqrt(x)
+print(x,y)
