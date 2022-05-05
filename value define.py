e15509a4b0c35009213222181be1b1beee2324c0
Solanki-Nikita <90173555+Solanki-Nikita@users.noreply.github.com>
2022-05-05 22:48:25 +0530

    Add files via upload

---
+++ value define.py
@@ -0,0 +1,11 @@
+def user_input():
+    a=input("Enter value 1 :")
+    b=input("Enter value 2 :")
+    retrun (a,b)
+
+def add (a,b):
+    return (a+b)
+
+    a,b=user_input()
+    #add(a,b)
+    print(a,'+',b,'=',add(a,b))
