27d1a073092d6efc133ba0c33b67297c1233779f
Solanki-Nikita <90173555+Solanki-Nikita@users.noreply.github.com>
2022-05-05 23:00:11 +0530

    Add files via upload

---
+++ pythoon_string 1.py
@@ -0,0 +1,9 @@
+a=int(input("please enter a value:"))
+a=int(a)
+print(type(a))
+if(a==0):
+    print("a is neiher even nor odd")
+elif a%2==0:
+    print("a ia a even")
+else:
+    print("a is a odd")
