996772b48d5d27ae2f3b9aedf7a1c36933f79522
Solanki-Nikita <90173555+Solanki-Nikita@users.noreply.github.com>
2022-05-08 09:12:09 +0530

    Add files via upload

---
+++ create a dictonary  python.py
@@ -0,0 +1,26 @@
+##Python Program to create a dictionary
+
+
+'''Student = {"Name": "John", "Age": 10, "Result":"Passed","Rollno":"009001"}  
+print(type(Student))  
+print("printing Employee data .... ")  
+print(Student)   '''
+
+
+'''#dict = {}  
+print("Empty Dictionary is: ")  
+print(dict)  
+  
+# Creating a Dictionary  
+# using the dict() method  
+dict1 = dict({1: 'Hello', 2: 'Hi', 3: 'Hey'})  
+print("\nCreate Dictionary by using the dict() method : ")  
+print(dict1)  
+  
+# Creating a Dictionary  
+# with each item as a Pair  
+dict2 = dict([('Devansh', 90014), ('Arun', 90015)])  
+print("\nDictionary with each item as a pair: ")  
+print(dict2)  '''
+
+
