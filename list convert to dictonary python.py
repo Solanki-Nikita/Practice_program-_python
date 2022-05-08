3c4c2dda49defebcd6474e0d4cab120389776728
Solanki-Nikita <90173555+Solanki-Nikita@users.noreply.github.com>
2022-05-08 09:22:02 +0530

    Add files via upload

---
+++ list convert to dictonary python.py
@@ -0,0 +1,18 @@
+##Python Program to convert list to dictionary
+
+'''list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  
+square_dict = {n: n*n for n in list1}  
+print(square_dict)  '''
+
+
+'''def Convert_dict(a):  
+    init = iter(list1)  
+    res_dct = dict(zip(init, init))  
+    return res_dct  
+  
+  
+# Driver code  
+list1 = ['x', 1, 'y', 2, 'z', 3]  
+print(Convert_dict(list1))  '''
+
+
